bce7ca4bb9650b8bb46c2a2546f0cd917518a32c
Kaushik das <kaushik1641.cse19@chitkara.edu.in>
2023-01-22 15:38:09 +0530

    project1

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import WorkExperience from '@/components/WorkExperience'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 // import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -33,6 +34,10 @@ export default function Home() {
 
       <section id='skills' className='snap-start'>
         <Skills/>
+      </section>
+
+      <section id='projects' className='snap-start'>
+          <Projects />
       </section>
 
 
